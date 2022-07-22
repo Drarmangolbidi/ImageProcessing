42f520acf5f23c6c1f6e85fddacfdd0e549f8cd1
Arman Golbidi <drarmangolbidi@gmail.com>
2022-07-22 19:51:05 +0430

    Update 5.CropPic.py

--- 5.CropPic.py
+++ 5.CropPic.py
@@ -1,24 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
 
 import cv2
-
-
-# In[7]:
-
-
+import cv2
 img = cv2.imread('Arman.jpg')
 img = img[0:400 , 0:350]
 cv2.imshow('mypic', img) 
 cv2.waitKey() 
 cv2.destroyAllwindows()
-
-
-# In[ ]:
-
 
 
 
